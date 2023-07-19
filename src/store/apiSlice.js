18a75948e35a7825793b9e72e3b6ed920a5c9f3e
Tarun <tarunpahade6969@gmail.com>
2023-07-19 14:52:20 +0530

    Bug Fixes

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-//const baseUrl = "https://backend-5ig7.onrender.com/";
-const baseUrl = "http://192.168.106.253:5000/";
+const baseUrl = "https://backend-5ig7.onrender.com/";
+//const baseUrl = "http://192.168.106.253:5000/";
 
 export const appSlice = createApi({
   reducerPath: "api",
